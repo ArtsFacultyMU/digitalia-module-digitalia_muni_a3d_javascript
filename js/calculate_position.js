9df9e325f85c21b4e71bbdc2f06735b40321e55e
xadler1 <469190@mail.muni.cz>
2025-05-07 11:15:03 +0200

    Alter links on ALL eligible elements, fix code formatting

--- js/calculate_position.js
+++ js/calculate_position.js
@@ -1,7 +1,7 @@
 function alter_links(context, settings)
 {
-  const RESULTS_PER_PAGE = settings.digitaliaMuniA3dJavascript.calculatePosition['items_per_page'];
-  const DISPLAY_FORMAT = settings.digitaliaMuniA3dJavascript.calculatePosition['display_format'];
+  const RESULTS_PER_PAGE = settings.digitaliaMuniA3dJavascript.calculatePosition["items_per_page"];
+  const DISPLAY_FORMAT = settings.digitaliaMuniA3dJavascript.calculatePosition["display_format"];
   const PAGER_ID = "0%2C";
 
   let separator = "&";
@@ -25,18 +25,21 @@ function alter_links(context, settings)
 
 
   if (DISPLAY_FORMAT == "grid") {
-    const grid_sections = context.querySelectorAll('.region-content .view-content .views-view-grid');
+    const grid_sections = context.querySelectorAll(".region-content .view-content .views-view-grid");
 
     for (let i = 0; i < grid_sections.length; i++) {
-      items = grid_sections[i].getElementsByClassName('views-col');
+      items = grid_sections[i].getElementsByClassName("views-col");
+
       for (let j = 0; j < items.length; j++) {
         link = paged_search_url + PAGER_ID + (page_number * RESULTS_PER_PAGE + counter);
-        tags = items[j].getElementsByClassName('field-content');
+        tags = items[j].getElementsByClassName("field-content");
+
         for (let k = 0; k < tags.length; k++) {
-            if (tags[k].firstChild) {
-              tags[k].children[0].setAttribute('href', link);
+            if (tags[k].firstChild && tags[k].firstChild instanceof Element) {
+              tags[k].firstChild.setAttribute("href", link);
             }
         }
+
         counter += 1;
       }
     }
@@ -46,15 +49,21 @@ function alter_links(context, settings)
     const tables = context.querySelectorAll("table.views-view-table tbody");
 
     for (let i = 0; i < tables.length; i++) {
-        let table_rows = tables[i].querySelectorAll("tr");
-        for (let j = 0; j < table_rows.length; j++) {
+      let table_rows = tables[i].querySelectorAll("tr");
+
+      for (let j = 0; j < table_rows.length; j++) {
             link = paged_search_url + PAGER_ID + (page_number * RESULTS_PER_PAGE + counter);
-        let element = table_rows[j].querySelector("td.views-field-field-artefact-id a");
-        if (element) {
-          element.setAttribute('href', link);
+
+        let tags = table_rows[j].querySelectorAll("td.views-field a");
+
+        for (let k = 0; k < tags.length; k++) {
+          if (tags[k] && tags[k] instanceof Element) {
+            tags[k].setAttribute("href", link);
+          }
         }
-            counter += 1;
-        }
+
+        counter += 1;
+      }
     }
   }
 }
